75eb5be2d8c1816ea1b1a881b3ee82b84e61349e
imno <krzysztof.jan.andrzejewski@gmail.com>
2025-06-24 23:02:35 +0200

    a

--- include/lib.h
+++ include/lib.h
@@ -1,14 +1,22 @@
 #ifndef TMB_LIB
 #define TMB_LIB
 
-#ifdef _WIN32
-  #ifdef TMB_EXPORTS
-    #define TMB_API __declspec(dllexport)
-  #else
-    #define TMB_API __declspec(dllimport)
-  #endif
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #ifdef TMB_SHARED
+        #ifdef TMB_EXPORTS
+            #define TMB_API __declspec(dllexport)
+        #else
+            #define TMB_API __declspec(dllimport)
+        #endif
+    #else
+        #define TMB_API
+    #endif
 #else
-  #define TMB_API
+    #if defined(TMB_EXPORTS)
+        #define TMB_API __attribute__((visibility("default")))
+    #else
+        #define TMB_API
+    #endif
 #endif
 
 TMB_API int VERY_SPECIAL_FUNCTION_THAT_IS_IMPLEMENTED_IN_LIB_C(int b);
